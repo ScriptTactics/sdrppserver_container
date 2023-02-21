0cdef1a0b73b6e6abcf7229cba9db1dc9881eb8e
PWRxPSYCHO <pwrxpsycho@gmail.com>
2023-02-20 23:03:14 -0500

    small tweaks

--- runcontainer.sh
+++ runcontainer.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
- docker run -d -p 5259:5259 --restart unless-stopped --name='sdrppserver' --device=/dev/bus/usb/002 --volume=/home/$USER/sdrpp:/config sdrppserver_local
+USB=$1
+
+sudo docker run -d -p 5259:5259 --restart unless-stopped --name='sdrppserver' --device=/dev/bus/usb/$USB --volume=/home/$USER/sdrpp:/config ghcr.io/scripttactics/sdrppserver_container:latest
